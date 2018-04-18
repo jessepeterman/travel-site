d0560fd569ad7de1a9e1bd28c9f5c2bfdf392433
Jesse Peterman <jessepeterman@Jesses-MacBook-Pro.local>
2018-04-18 11:13:22 -0700

    committing progress so far

---
+++ gulp/tasks/watch.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+browserSync = require('browser-sync').create();
+
+gulp.task('watch', function() {
+
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "app"
+    }
+  });
+
+  gulp.watch('./app/index.html', function() {
+    browserSync.reload();
+  });
+
+  gulp.watch('./app/assets/**/*.css', function() {
+    gulp.start('cssInject');
+    // gulp.start('');
+  });
+
+});
+
+gulp.task('cssInject', ['styles'], function() {
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
+});
