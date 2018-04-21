c541deb4238f5daf27661dd5e085d289edf0366b
jessepeterman <jessepeterman@gmail.com>
2018-04-21 14:54:00 -0700

    finished reavel on scroll feature

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,10 @@
 import MobileMenu from './modules/MobileMenu.js'
+import RevealOnScroll from './modules/RevealOnScroll.js';
+import $ from 'jquery';
+
 
 var mobileMenu = new MobileMenu();
+// var revealOnScroll = new RevealOnScroll();
+
+new RevealOnScroll($(".testimonial"), "80%");
+new RevealOnScroll($(".feature-item"), "85%");
