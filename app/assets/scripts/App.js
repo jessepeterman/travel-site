d48908aa11682bdf8e7d250bc0f55aac2e21665e
jessepeterman <jessepeterman@gmail.com>
2018-04-21 18:11:33 -0700

    fixed weird issue with js not loading properly...not sure what it was. Went back to an older version in GIT and started from there.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,10 +1,11 @@
 import MobileMenu from './modules/MobileMenu.js'
 import RevealOnScroll from './modules/RevealOnScroll.js';
+import StickyHeader from './modules/StickyHeader.js';
 import $ from 'jquery';
 
 
 var mobileMenu = new MobileMenu();
 // var revealOnScroll = new RevealOnScroll();
-//
+var stickyHeader = new StickyHeader();
 new RevealOnScroll($(".testimonial"), "80%");
 new RevealOnScroll($(".feature-item"), "85%");
