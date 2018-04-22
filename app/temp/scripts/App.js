e57d3147a9423d4d518dad26efee0abb6a6ae915
jessepeterman <jessepeterman@gmail.com>
2018-04-21 18:03:14 -0700

    currently loading .js files and reveal on scroll is working

--- app/temp/scripts/App.js
+++ app/temp/scripts/App.js
@@ -9908,8 +9908,8 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 var mobileMenu = new _MobileMenu2.default();
 // var revealOnScroll = new RevealOnScroll();
 //
-// new RevealOnScroll($(".testimonial"), "80%");
-// new RevealOnScroll($(".feature-item"), "85%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".testimonial"), "80%");
+new _RevealOnScroll2.default((0, _jquery2.default)(".feature-item"), "85%");
 
 /***/ }),
 /* 2 */
